6fa2d4c641e7d3f3bb0faa6a4b3290f7077086ac
Ryan Christie <ryan.christie@bbd.co.za>
2025-07-26 18:41:51 +0200

    Added routing and react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <h1>Frontend Setup</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Frontend setup</h1>} />
+      <Route path="/placeholder" element={<h1>Frontend setup page 2</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
