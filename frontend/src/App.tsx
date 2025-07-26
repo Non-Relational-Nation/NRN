d5dbb380b44983e7fd4e0f42f8b39f4865fb4a77
Ryan Christie <ryan.christie@bbd.co.za>
2025-07-26 19:02:18 +0200

    Added default route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Page1 from "./pages/Page1/Page1";
 import Page2 from "./pages/Page2/Page2";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Page1 />} />
       <Route path="/page2" element={<Page2 />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
