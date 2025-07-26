20ed06b348e9feee92ac5f42061ecb8868c986ab
Ryan Christie <ryan.christie@bbd.co.za>
2025-07-26 18:48:58 +0200

    Setting up folder structure + added some util

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Page1 from "./pages/Page1/Page1";
+import Page2 from "./pages/Page2/Page2";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Frontend setup</h1>} />
-      <Route path="/placeholder" element={<h1>Frontend setup page 2</h1>} />
+      <Route path="/" element={<Page1 />} />
+      <Route path="/page2" element={<Page2 />} />
     </Routes>
   );
 }
