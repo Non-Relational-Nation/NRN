24980c3c1f2cfe91b53b5d109b6c9ddc015f52c1
Ryan Christie <ryan.christie@bbd.co.za>
2025-07-28 23:08:50 +0200

    Neatened login functionality

--- frontend/src/pages/Login/Callback.tsx
+++ frontend/src/pages/Login/Callback.tsx
@@ -1,9 +1,33 @@
 import { handleOAuthCallback } from "../../api/login";
 import Loader from "../../components/Loader/Loader";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Callback() {
-  handleOAuthCallback();
-  return <Loader />;
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await handleOAuthCallback();
+        navigate("/");
+      } catch {
+        setError(true);
+      }
+    })();
+  }, [navigate]);
+
+  if (error) {
+    return (
+      <div>
+        <h2>Login failed</h2>
+        <button onClick={() => navigate("/login")}>Return to Login</button>
+      </div>
+    );
+  } else {
+    return <Loader />;
+  }
 }
 
 export default Callback;
