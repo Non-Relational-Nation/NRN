d2b5015a430c0df860185f54f4dcf78b216197ff
Ryan Christie <ryan.christie@bbd.co.za>
2025-07-30 23:26:08 +0200

    Basic feed

---
+++ frontend/src/components/Posts/PostList.tsx
@@ -0,0 +1,33 @@
+import Post from "./Post";
+import "./styles.css";
+
+export default function PostList() {
+  return (
+    <section id="post-container">
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+      <Post></Post>
+    </section>
+  );
+}
