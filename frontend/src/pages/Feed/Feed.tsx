81d4bb58c0cb64b17d287942d70f51fd635b9683
Ryan Christie <ryan.christie@bbd.co.za>
2025-07-30 23:26:08 +0200

    Basic feed

--- frontend/src/pages/Feed/Feed.tsx
+++ frontend/src/pages/Feed/Feed.tsx
@@ -1,11 +1,12 @@
 import "./styles.css";
 
 import Layout from "../../components/Layout/Layout";
+import PostList from "../../components/Posts/PostList";
 
 export default function Feed() {
   return (
     <Layout>
-      <h1>Feed</h1>
+      <PostList></PostList>
     </Layout>
   );
 }
