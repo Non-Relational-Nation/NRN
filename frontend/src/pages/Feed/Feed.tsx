bd7001628c0900cad3216c75f4e4a08e0cec147b
Ryan Christie <ryan.christie@bbd.co.za>
2025-07-30 22:55:50 +0200

    Added layout to all pages

--- frontend/src/pages/Feed/Feed.tsx
+++ frontend/src/pages/Feed/Feed.tsx
@@ -1,9 +1,11 @@
-import "./styles.css"
+import "./styles.css";
 
 import Layout from "../../components/Layout/Layout";
 
 export default function Feed() {
   return (
-    <Layout loading={false}><h1>Feed</h1></Layout>
+    <Layout>
+      <h1>Feed</h1>
+    </Layout>
   );
 }
--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,7 +1,8 @@
+import Layout from "../../components/Layout/Layout";
 import "./styles.css"
 
 export default function Profile() {
   return (
-    <h1>Profile</h1>
+    <Layout><h1>Profile</h1></Layout>
   );
 }
