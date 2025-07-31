aeea37d6f99cf59f9b8748505c5e724be7fe4cd7
ndzalama.mabasa <ndzalama.mabasa@bbd.co.za>
2025-07-31 19:40:42 +0200

    add federation setup

---
+++ backend/src/federation.ts
@@ -0,0 +1,32 @@
+import { createFederation, Endpoints, Person } from "@fedify/fedify";
+import { MemoryKvStore, InProcessMessageQueue } from "@fedify/fedify";
+import { UserModel } from "./models/userModel.ts";
+
+const federation = createFederation({
+  kv: new MemoryKvStore(), // to be changed to Redis
+  queue: new InProcessMessageQueue(), // to be changed to Redis
+});
+
+federation.setActorDispatcher(
+  "/users/{identifier}",
+  async (ctx, identifier) => {
+    const user = await UserModel.findOne({ username: identifier });
+
+    if (!user) return null;
+
+    return new Person({
+      id: ctx.getActorUri(identifier),
+      preferredUsername: identifier,
+      name: user.name,
+      inbox: ctx.getInboxUri(identifier),
+      endpoints: new Endpoints({
+        sharedInbox: ctx.getInboxUri(),
+      }),
+      url: ctx.getActorUri(identifier),
+    });
+  }
+);
+
+federation.setInboxListeners("/users/{identifier}/inbox", "/inbox");
+
+export default federation;
