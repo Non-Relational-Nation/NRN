dfff628d7c88330552d631cd3e39f862dd7e4c4d
Lindiwe Tshabalala <lindiwe@bbd.co.za>
2025-08-03 00:52:11 +0200

    Fix infrastructure

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -11,6 +11,6 @@ export const GOOGLE_REDIRECT_URL = window.location.hostname === "localhost"
 
 const LOCAL_API_URL = "http://localhost:3001";
 // Use environment variable set during build process
-const HOSTED_API_URL = (import.meta as any).env?.VITE_API_URL || "https://nrn-alb-grad-group01-dev-205573181.af-south-1.elb.amazonaws.com/api";
+const HOSTED_API_URL = (import.meta as any).env?.VITE_API_URL || "https://d19q1152kff9b0.cloudfront.net";
 
 export const API_URL = window.location.hostname === "localhost" ? LOCAL_API_URL : HOSTED_API_URL;
