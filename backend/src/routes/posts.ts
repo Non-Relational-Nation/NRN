9546c4cea3ac5df07c32fabde9606b9b66944113
Brilliant Makhoba <Manqoba.Makhoba@bbd.co.za>
2025-08-02 17:20:26 +0200

    Add: posts route for post-related API endpoints

---
+++ backend/src/routes/posts.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { PostController } from '../controllers/PostController.js';
+import { postRepository } from '../repositories/postRepository.js';
+import { userRepository } from '../repositories/userRepository.js';
+import { PostService } from '../services/postService.js';
+import { upload } from '../middleware/multerUpload.js';
+
+const router = Router();
+const postService = new PostService(postRepository, userRepository);
+const postController = new PostController(postService);
+
+router.post('/', upload.array('files'), (req, res) => postController.createPost(req, res));
+router.get('/:id', (req, res, next) => postController.getPost(req, res, next));
+router.put('/:id', (req, res) => postController.updatePost(req, res));
+router.delete('/:id', (req, res) => postController.deletePost(req, res));
+router.get('/author/:authorId', (req, res, next) => postController.getPostsByAuthor(req, res, next));
+router.get('/', (req, res, next) => postController.getPublicPosts(req, res, next));
+router.get('/search', (req, res) => postController.searchPosts(req, res));
+router.get('/feed/:userId', (req, res) => postController.getFeed(req, res));
+router.post('/:id/like', (req, res) => postController.likePost(req, res));
+
+export { router as postRoutes };
