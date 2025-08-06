df097e3f4bd39ff45230117e2a00b521b97efd5b
Brilliant Makhoba <Manqoba.Makhoba@bbd.co.za>
2025-08-06 22:21:03 +0200

    logger

--- backend/src/federation/federationContext.ts
+++ backend/src/federation/federationContext.ts
@@ -1,8 +1,17 @@
 import type { Request as ExpressRequest } from "express";
 import federation from "../federation.ts";
+import winston from 'winston';
 
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
 export function createFederationContextFromExpressReq(req: ExpressRequest) {
-  console.log("[federationContext] Request headers:", req);
+  logger.log("[federationContext] Request headers:", req);
   const protocol = "https";//req.get("X-Forwarded-Proto") || req.protocol;
   const fullUrl = 'https://dikiudmyn4guv.cloudfront.net';
   const url = new URL(fullUrl);
